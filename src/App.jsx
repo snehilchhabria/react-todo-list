3826f478cd083439cc8e6356054840125b5c3f5b
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-20 19:03:36 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-item-form">
-  <div className="form-row">
-  <label>New Item</label>
-  <input type="text" id= "item"/>
-  </div>
-  </form>
+  return (
+    <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+    </>
+  )
 }
