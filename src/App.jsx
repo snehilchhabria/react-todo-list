b1ab98e8ff25a41724cf2c99a029ba2dd745e97b
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-20 19:18:37 +0530

    useState usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("") //newItem is the fixed value and setNewItem is the new value we will keep mpdifying while using
   return (
     <>
     <form className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
+        <input value={newItem} onChange={e => setNewItem(e.target.value)}
+        type="text" id="item" />
       </div>
     </form>
     <h1 className="header">Todo List</h1>
