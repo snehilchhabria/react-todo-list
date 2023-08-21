86f33ea71af2b6c0985e63b6b77a81a906a8cd79
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-22 00:10:25 +0530

    components structuring

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export function NewTodoForm() {
+    const [newItem, setNewItem] = useState("") //newItem is the fixed value and setNewItem is the new value we will keep mpdifying while using
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if(newItem === "") return
+        addTodo(newItem)
+        setNewItem("")
+      }
+
+    return (
+        <form onSubmit ={handleSubmit}className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem} onChange={e => setNewItem(e.target.value)}
+        type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    )
+}
