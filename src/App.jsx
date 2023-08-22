0f5fbbbc42ffd3f3312a852e4d482cf63ce97cd3
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-22 15:03:41 +0530

    persisting the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
 import { TodoList } from "./TodoList"
 
 export default function App() {
 
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(()=> {
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  })
   
   function addTodo(title) {
     setTodos(currentTodos => {
@@ -36,7 +44,7 @@ export default function App() {
     <>
     <NewTodoForm onSubmission={addTodo}/>
     <h1 className="header">Todo List</h1>
-    <TodoList todos={todos}/>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
     </>
   )
 }
